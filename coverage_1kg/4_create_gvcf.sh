34778c7a6721db763fd7c8c313d4f54d2d25c88d
arbarton <83524351+arbarton@users.noreply.github.com>
2023-02-26 15:54:16 -0500

    Update 4_create_gvcf.sh

--- coverage_1kg/4_create_gvcf.sh
+++ coverage_1kg/4_create_gvcf.sh
@@ -10,12 +10,12 @@ X=${SLURM_ARRAY_TASK_ID}
 
 module load gatk
 
-
+#remove any old files from previous runs
 rm ./run_${Y}/merge.list
 rm ./run_${Y}/sample_${X}/sample_${X}.L.g.vcf.gz
 rm ./run_${Y}/sample_${X}/sample_${X}.L.g.vcf.gz.tbi
 
-
+#index bams and run haplotype caller on per-fraction bam
 sed -i '/V1/d' ./run_${Y}/sample_${X}/sample${X}.frac1.bam.bed
 for bam in ./run_${Y}/sample_${X}/*bam; do
 samtools index ${bam}
@@ -23,10 +23,13 @@ gatk HaplotypeCaller -ERC GVCF -I ${bam}  -R GRCh38_full_analysis_set_plus_decoy
 done
 ls ./run_${Y}/sample_${X}/*.L.g.vcf.gz > ./run_${Y}/sample_${X}/gvcf.list
 
+#combine by-fraction gvcfs for each sample
 gatk CombineGVCFs  -R GRCh38_full_analysis_set_plus_decoy_hla.fa  --variant ./run_${Y}/sample_${X}/gvcf.list -O  ./run_${Y}/sample_${X}/sample_${X}.L.g.vcf.gz
 
+#create list of gvcfs to combine for genotyping
 echo ./run_${Y}/sample_${X}/sample_${X}.L.g.vcf.gz >> ./run_${Y}/merge.list
 
+#remove larger intermediate files
 rm ./run_${Y}/sample_${X}/sample${X}.frac*.L.g.vcf.gz
 rm ./run_${Y}/sample_${X}/sample${X}.frac*.L.g.vcf.gz.tbi
 rm ./run_${Y}/sample_${X}/sample${X}.*bam.bed
